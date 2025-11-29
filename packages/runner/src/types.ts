efdba522675fce1e1d9108b065a37f9e3b413da5
Ladislav Zigo <ladislav.zigo@almamedia.com>
2025-11-29 11:51:14 +0100

    refactor: simplify RunnerAdapter interface and update RunnerAdapterFactory type definition for improved clarity

--- packages/runner/src/types.ts
+++ packages/runner/src/types.ts
@@ -1,8 +1,5 @@
 import { PipelineDefinition, PipelineResult, RunPipelineOptions } from "@virta/core";
-
-export interface RunnerAdapter<S, T> {
-  run(definition: PipelineDefinition<S, T>, options: RunnerOptions<S, T>): Promise<PipelineResult<S, T>>;
-}
+import type { RunnerAdapter } from "./RunnerAdapter.js";
 
 export interface RunnerOptions<S, T> extends RunPipelineOptions<S, T> {
   // Additional options can be added here
@@ -11,5 +8,5 @@ export interface RunnerOptions<S, T> extends RunPipelineOptions<S, T> {
   checkpoint?: string; // Serialized checkpoint to resume from
 }
 
-export type RunnerAdapterFactory = () => RunnerAdapter<any, any>;
+export type RunnerAdapterFactory = () => RunnerAdapter;
 
